8d90eb22716276efc61ce5f82506035387287237
Pooja <poojavpatel71@gmail.com>
2020-12-03 14:53:21 +0530

    changed logic to figure out parent sheet,maintain last visited level,changed few hardcoded values

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from openpyxl import load_workbook
 path = 'data.xlsx'
 wb=load_workbook(path)
 
-level = {
-  ".1" : {'row':0, 'column':-1},
-  "..2" : {'row':-1, 'column':1},
-  "..3" : {'row':-1, 'column':1}
+lastVisitedLevel = {
+  '.1' : False,
+  '..2' : False,
+  '..3' : False
 }
+offset = 2
 
 # Function to create a sheet
 def createSheet(sheet):
@@ -33,21 +34,33 @@ def completeSheet(sheet):
   wb.save(path)
   return currentsheet
 
+def parentCell(level, item):
+  if(level == '.1'):
+    return item
+  if(level == '..2'):
+    return lastVisitedLevel['.1']
+  if(level == '..3'):
+    return lastVisitedLevel['..2']
+  return None
+
 sheet=wb.active
-for i in range(2,13):   
-  offset = 2
-  cell=sheet.cell(row=i,column=offset)
-  itemCell = sheet.cell(row=i,column=2+1)
-  shift = level[cell.value]
+
+for i in range(2,sheet.max_row):
+  if(sheet.cell(row=i,column=offset).value is None):
+    break
+  levelCell=sheet.cell(row=i,column=offset)
+  materialCell = sheet.cell(row=i,column=offset+1)
+  lastVisitedLevel[levelCell.value] = materialCell.value
+  itemCell = sheet.cell(row=i,column=offset-1)
   # figure out parent sheet for multi level BOM
-  parentCell=sheet.cell(row=i+shift['row'],column=offset+shift['column'])
+  parent=parentCell(levelCell.value, itemCell.value)
   quantityCell=sheet.cell(row=i,column=offset+2)
   unitCell=sheet.cell(row=i,column=offset+3)
   # If parent sheet does not exist, create new and add values
-  if(parentCell.value not in wb.sheetnames):
-    createSheet(parentCell.value)
-  currentsheet = wb.get_sheet_by_name(parentCell.value)
-  currentsheet.append((1,itemCell.value,quantityCell.value,unitCell.value))
+  if(parent not in wb.sheetnames):
+    createSheet(parent)
+  currentsheet = wb.get_sheet_by_name(parent)
+  currentsheet.append((1,materialCell.value,quantityCell.value,unitCell.value))
   wb.save(path)
 
 # Closing all sheets created except the source sheet
